452361072bdb9a4be73c79384b24833b49a9959d
Lucas Cunha <lucas_cunha_souza@hotmail.com>
2023-11-10 10:19:24 -0300

    Teste

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,15 @@ import org.example.model.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        vaiVimoErro();
+        vai_VimoErro();
     }
 
     public static void vai_VimoErro(){
+        Pessoa vai_quebrar = new Pessoa("Lucas", "123");
+    }
+    public static void vaiVimoErro(){
         Pessoa vai_quebrar = new Pessoa("Lucas", "123");
     }
 }
