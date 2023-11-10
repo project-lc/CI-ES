9a19052859159cf1b67799b79b0e3dc4b3e61375
Lucas Cunha de Souza <56975597+project-lc@users.noreply.github.com>
2023-11-10 09:44:25 -0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static void vaiVimoErro(){
+        Pessoa vaiquebrar = new Pessoa();
+    }
 }
